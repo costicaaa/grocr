2092a5b836a973f40f6fb26aaefd0c1e4ac2dbe9
costin <cosraducostache@gmail.com>
2020-04-18 17:05:46 +0300

    push for project

--- Grocr/Controllers/AvailableCouriersViewController.swift
+++ Grocr/Controllers/AvailableCouriersViewController.swift
@@ -9,6 +9,7 @@ class AvailableCouriersViewController: UITableViewController {
   // MARK: Properties
   var currentUsers: [String] = []
   let user = Auth.auth().currentUser!
+    var items: [GroceryItem] = []
 
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
@@ -17,30 +18,58 @@ class AvailableCouriersViewController: UITableViewController {
   // MARK: UIViewController Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+
+
+   
     
     let couriersRef = Database.database().reference().child("users-info").queryOrdered(byChild: "type").queryEqual(toValue: 2)
-    couriersRef.observe(.childAdded, with: { snap in
-      let temp = snap.value as? NSDictionary
-      self.currentUsers.append(temp?["userName"] as? String ?? "")
-      let row = self.currentUsers.count - 1
-      let indexPath = IndexPath(row: row, section: 0)
-      self.tableView.insertRows(at: [indexPath], with: .top)
-    })
+
+      couriersRef.observe(.value, with: { snapshot in
+                var newItems: [UserInfo] = []
+                for child in snapshot.children {
+                  if let snapshot = child as? DataSnapshot,
+                    let userInfoItem = UserInfo(snapshot: snapshot) {
+                    newItems.append(groceryItem)
+                  }
+                }
+                
+                self.items = newItems
+                self.tableView.reloadData()
+
+    // couriersRef.observe(.childAdded, with: { snap in
+    //   let temp = snap.value as? NSDictionary
+    //   self.currentUsers.append(temp?["userName"] as? String ?? "")
+    //   let row = self.currentUsers.count - 1
+    //   let indexPath = IndexPath(row: row, section: 0)
+    //   self.tableView.insertRows(at: [indexPath], with: .top)
+    // })
 
   }
   
   // MARK: UITableView Delegate methods
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return currentUsers.count
+    return items.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: userCell, for: indexPath)
-    let onlineUserEmail = currentUsers[indexPath.row]
+    let onlineUserEmail = items[indexPath.row]
     cell.textLabel?.text = onlineUserEmail
     return cell
   }
   
   // MARK: Actions
- 
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else { return }
+    let userInfoItem = items[indexPath.row]
+    // let toggledCompletion = !groceryItem.completed
+    toggleCellCheckbox(cell)
+    // groceryItem.ref?.updateChildValues([
+    //   "completed": toggledCompletion
+    //   ])
+  }
+
+  func toggleCellCheckbox(_ cell: UITableViewCell) {
+    print("clicked a row")
+  }
 }
