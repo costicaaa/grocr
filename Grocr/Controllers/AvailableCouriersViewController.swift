5251b3e958dae381d10279d93686a047b437fa5b
costin <cosraducostache@gmail.com>
2020-04-19 03:11:47 +0300

    push for project

--- Grocr/Controllers/AvailableCouriersViewController.swift
+++ Grocr/Controllers/AvailableCouriersViewController.swift
@@ -9,8 +9,10 @@ class AvailableCouriersViewController: UITableViewController {
   // MARK: Properties
   var currentUsers: [String] = []
   let user = Auth.auth().currentUser!
-    var items: [UserInfo] = []
-
+  var items: [UserInfo] = []
+  var groceryItems : [GroceryItem] = []
+  var ref = Database.database.reference()
+  var newOrderItems: [GroceryItem] = []
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
   }
@@ -69,21 +71,58 @@ class AvailableCouriersViewController: UITableViewController {
     func toggleCellCheckbox(_ cell: UITableViewCell, userInfoItem: UserInfo) {
       print("clicked a row")
         // print(userInfoItem.key)
-        print(userInfoItem.userName)
+      print(userInfoItem.userName)
 
-          let alert = UIAlertController(title: "Send order to " + userInfoItem.userName,
-                                  message: "Are you sure?",
-                                  preferredStyle: .alert)
+      let alert = UIAlertController(title: "Send order to " + userInfoItem.userName,
+                              message: "Are you sure?",
+                              preferredStyle: .alert)
         
 
       let cancelAction = UIAlertAction(title: "Cancel",
                                         style: .cancel)
 
       let saveAction = UIAlertAction(title: "Send", style: .default) { _ in
-        print("now send order")
-        }
+
+        let ref = Database.database().reference()
+        ref.child("users-info").child(user.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+          // Get user value
+          let currentUserInfo = snapshot.value as? NSDictionary
+          let familyUID = value?["familyUID"] as? String ?? ""
+          let newOrderKey = self.ref.child("families").child(familyUID).child("orders").childByAutoId().key
+          self.ref.child("families").child(familyUID).child("orders").childByAutoId().setValue([
+            "deliveryUserUID": userInfoItem.key,
+            "deliveryUserName": userInfoItem.userName,
+            "forFamilyUID": userInfoItem.familyUID,
+            "status": "processing",
+            "groceryItems" : []
+          ])
+
+          for newOrderItem in self.newOrderItems {
+            self.ref.child("families").child(familyUID).child("orders").child("groceryItems").childByAutoId().setValue(newOrderItem.toAnyObject())
+          }
+                
+        })
+      })
+        
+      
+
+      
+
+
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
+
+      self.newOrderItems = []
+      self.ref = Database.database().reference(withPath: "families/\(value?["familyUID"] as? String ?? "" )/grocery-items")
+      
+      self.ref.queryOrdered(byChild: "addedByUser").observe(.value, with: { snapshot in
+        
+        for child in snapshot.children {
+          if let snapshot = child as? DataSnapshot,
+            let orderItem = GroceryItem(snapshot: snapshot) {
+            self.newOrderItems.append(orderItem)
+          }
+        }
         
        self.present(alert, animated: true, completion: nil)
     }
